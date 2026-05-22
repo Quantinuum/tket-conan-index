07c6925a9f7ef7b3a5800eb3a3d44a1659155bb9
Alec Edgington <alec.edgington@quantinuum.com>
2026-05-22 08:48:36 +0100

    Update cmake requirement for catch2 recipe.

--- recipes/catch2/conanfile.py
+++ recipes/catch2/conanfile.py
@@ -68,7 +68,7 @@ class Catch2Conan(ConanFile):
 
     def build_requirements(self):
         if Version(self.version) >= "3.8.0":
-            self.tool_requires("cmake/[>=3.16 <4]")
+            self.tool_requires("cmake/[>=4.3 <5]")
 
     def validate(self):
         check_min_cppstd(self, 14)
