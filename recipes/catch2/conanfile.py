3a29f1a81b434a9dc407c233a7642e370f517b69
Alec Edgington <alec.edgington@quantinuum.com>
2026-09-10 11:35:11 +0100

    Add catch2 3.16.0.

--- recipes/catch2/conanfile.py
+++ recipes/catch2/conanfile.py
@@ -16,7 +16,7 @@ required_conan_version = ">=2.8"
 
 class Catch2Conan(ConanFile):
     name = "catch2"
-    version = "3.15.3"
+    version = "3.16.0"
     description = (
         "A modern, C++-native, header-only, framework for unit-tests, TDD and BDD"
     )
